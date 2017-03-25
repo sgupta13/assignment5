747468732f09d8190ead4b59fdb0e11010afbb45
sgupta13 <sgupta13@student.gsu.edu>
2017-03-25 12:41:41 -0400

    assignment5

--- Form_check.js
+++ Form_check.js
@@ -1,65 +1,81 @@
 function checkCompleteness()
  {
-   if(document.getElementById("userID").value == "")
+   if(document.getElementById("userID").value.length == 0)
    {
     var errmsg= document.getElementById('errID');
     errmsg.style.color="red";
     errmsg.style.textDecoration = "underline";
-    document.getElementById('errID').textContent ="          Please enter an ID";
+    document.getElementById('errID').textContent ="Please enter an ID";
    }
    
-  if(document.getElementById("userFN").value == "" )
+  if(document.getElementById("userFN").value.length == 0)
    {
     var errmsg= document.getElementById('errFN');
     errmsg.style.color="red";
     errmsg.style.textDecoration = "underline";
-    document.getElementById('errFN').textContent ="          Please enter a First Name";
+    document.getElementById('errFN').textContent ="Please enter a First Name";
    }
 
-  if(document.getElementById("userLN").value == "")
+  if(document.getElementById("userLN").value.length == 0)
    {
     var errmsg= document.getElementById('errLN');
     errmsg.style.color="red";
     errmsg.style.textDecoration = "underline";
-    document.getElementById('errLN').textContent ="          Please enter a Last Name";
+    document.getElementById('errLN').textContent ="Please enter a Last Name";
    }
    
-  if(document.getElementById("userID").value == "" && document.getElementById("userFN").value !=0 && document.getElementById("userLN").value !=0 )
+  if (document.getElementById("userID").value.length == 0 
+   && document.getElementById("userFN").value.length != 0 
+   && document.getElementById("userLN").value.length != 0)
    {
     alert("You forgot to enter the following field(s) ID");
    }
 
-  if(document.getElementById("userID").value !=0 && document.getElementById("userFN").value == "" && document.getElementById("userLN").value !=0 )
+  if (document.getElementById("userID").value.length != 0 
+   && document.getElementById("userFN").value.length == 0 
+   && document.getElementById("userLN").value.length != 0)
    {
     alert("You forgot to enter the following field(s) FirstName");
    }
 
-  if(document.getElementById("userID").value !=0 && document.getElementById("userFN").value !=0 && document.getElementById("userLN").value == "" )
+  if (document.getElementById("userID").value.length != 0 
+   && document.getElementById("userFN").value.length != 0 
+   && document.getElementById("userLN").value.length == 0)
    {
     alert("You forgot to enter the following field(s) LastName");
    }
   
-  if(document.getElementById("userID").value == "" && document.getElementById("userFN").value == "" && document.getElementById("userLN").value !=0)
+  if (document.getElementById("userID").value.length == 0 
+   && document.getElementById("userFN").value.length == 0 
+   && document.getElementById("userLN").value.length != 0)
    {
     alert("You forgot to enter the following field(s) ID, FirstName");
    }
    
-  if(document.getElementById("userID").value == "" && document.getElementById("userFN").value !=0 && document.getElementById("userLN").value == "" )
+  if (document.getElementById("userID").value.length == 0 
+   && document.getElementById("userFN").value.length != 0 
+   && document.getElementById("userLN").value.length == 0)
    {
     alert("You forgot to enter the following field(s) ID, LastName");
    }
 
-   if(document.getElementById("userID").value !=0 && document.getElementById("userFN").value == "" && document.getElementById("userLN").value == "" )
+   if (document.getElementById("userID").value.length != 0 
+    && document.getElementById("userFN").value.length == 0 
+	&& document.getElementById("userLN").value.length == 0)
    {
     alert("You forgot to enter the following field(s) FirstName, LastName");
    }
 
-  if(document.getElementById("userID").value == "" && document.getElementById("userFN").value == "" && document.getElementById("userLN").value == "" )
+  if (document.getElementById("userID").value.length == 0 
+   && document.getElementById("userFN").value.length == 0 
+   && document.getElementById("userLN").value.length == 0)
    {
     alert("You forgot to enter the following field(s) ID ,FirstName, LastName");
    }
    
-  if(document.getElementById("userID").value !="" && document.getElementById("userFN").value !=""  && document.getElementById("userLN").value !="")
+  if (document.getElementById("userID").value.length != 0
+   && document.getElementById("userFN").value.length != 0
+   && document.getElementById("userLN").value.length != 0)
    {
     var inputID=document.getElementById("userID").value;
     var inputFN=document.getElementById("userFN").value;
